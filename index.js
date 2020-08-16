cfd1c5cf6ecf2a45cb276ef2415996cbe37b7849
speedster01 <akshay.singh1403@gmail.com>
2020-08-16 11:14:15 +0530

    Simple Node example

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect={
+  perimeter: (x,y) => (2*(x+y)),
+  area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+  console.log("Solving for Rectangle with l ="+l+ " and b ="+b);
+
+  if(l<=0 || b<=0){
+    console.log("Rectangle doesn't exist");
+  }
+  else{
+    console.log("The area of Rectangle is "+rect.area(l,b));
+    console.log("The perimeter of Rectangle is "+rect.perimeter(l,b));
+  }
+}
+
+solveRect(2,4);
+solveRect(0,4);
+solveRect(10,12);
+solveRect(-1,3);
